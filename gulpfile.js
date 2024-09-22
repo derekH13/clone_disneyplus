4bb4ba43250ef33687f6748f915c5c6063a7302f
derekH13 <168042164+derekH13@users.noreply.github.com>
2024-09-22 11:54:24 -0300

    criando o hero e importando a font avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -7,10 +8,26 @@ function styles(){
     .pipe(gulp.dest('./dist/css'));
 }
 
+function images(){
+    return gulp.src('./images/**/*', { encoding: false })
+        .pipe(imagemin([
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./dist/images'));
+}
 
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
+
+
 exports.watch = function(){
     // arquivos que seram observados, e função que sera executada
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles, images))
 }
